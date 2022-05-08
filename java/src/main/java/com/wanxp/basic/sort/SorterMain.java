d39420a76c1b849887e064eadee610abe67f70be
hughwan <977741432@qq.com>
2022-05-08 20:51:09 +0800

    main change

--- java/src/main/java/com/wanxp/basic/sort/SorterMain.java
+++ java/src/main/java/com/wanxp/basic/sort/SorterMain.java
@@ -4,7 +4,7 @@ import com.wanxp.basic.sort.ArrayIniter.Type;
 
 public class SorterMain {
 
-    static int length = 4;
+    static int length = 1000000;
     public static void main(String[] args) {
         boolean display = length < 100;
         
@@ -12,9 +12,11 @@ public class SorterMain {
         // new ArrayIniter(length, Type.RANDOM, display).sort(new BubbleSort());
         // new ArrayIniter(length, Type.RANDOM, display).sort(new SelectSort());
         // new ArrayIniter(length, Type.RANDOM, display).sort(new InsertSort());
-        // new ArrayIniter(length, Type.RANDOM, display).sort(new QuickSort());
-        // new ArrayIniter(length, Type.RANDOM, display).sort(new ShellSort());
-        // new ArrayIniter(length, Type.RANDOM, display).sort(new MergeSort());
-        new MergeSort().sort(new int[]{0,1,3,1});
+        new ArrayIniter(length, Type.RANDOM, display).sort(new QuickSort());
+        new ArrayIniter(length, Type.RANDOM, display).sort(new ShellSort());
+        new ArrayIniter(length, Type.RANDOM, display).sort(new MergeSort());
+        new ArrayIniter(length, Type.RANDOM, display).sort(new HeapSort());
+        new ArrayIniter(length, Type.RANDOM, display).sort(new HeapSort2());
+        // new HeapSort2().sort(new int[]{4,3,2,1});
     }
 }
