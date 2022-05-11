37a06796cf09cc93ec73e6e5eadda236a50b96ea
hughwan <977741432@qq.com>
2022-05-11 22:49:27 +0800

    优化 选择排序

--- java/src/main/java/com/wanxp/basic/sort/SelectSort.java
+++ java/src/main/java/com/wanxp/basic/sort/SelectSort.java
@@ -1,16 +1,26 @@
 package com.wanxp.basic.sort;
-public class SelectSort implements Sorter{
-    public long sort(int[] arr) {
-        long start = System.currentTimeMillis();
-        for(int i = arr.length - 1;i > 0;i--) {
-            for(int j = 0;j < i - 1;j++) {
-                if(arr[j] > arr[i]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
-        return System.currentTimeMillis() - start;
-    }
+public class SelectSort implements Sorter {
+	public long sort(int[] arr) {
+		long start = System.currentTimeMillis();
+		int minIndex;
+		for(int i = 0;i < arr.length;i++) {
+			minIndex = i;
+			for(int j = i;j < arr.length;j++) {
+				if(arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+			if(minIndex != i) {
+				swap(arr, i, minIndex);	
+			}
+		}
+		return System.currentTimeMillis() - start;
+	}
+
+	public void swap(int[] arr, int index1, int index2) {
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+
 }
--- java/src/main/java/com/wanxp/basic/sort/SorterMain.java
+++ java/src/main/java/com/wanxp/basic/sort/SorterMain.java
@@ -1,5 +1,6 @@
 package com.wanxp.basic.sort;
 
+import com.wanxp.basic.sort.ArrayIniter.Type;
 
 public class SorterMain {
 
@@ -11,15 +12,15 @@ public class SorterMain {
         
 
         // new ArrayIniter(length, Type.RANDOM, display).sort(new BubbleSort());
-        // new ArrayIniter(length, Type.RANDOM, display).sort(new SelectSort());
+        new ArrayIniter(length, Type.RANDOM, display).sort(new SelectSort());
         // new ArrayIniter(length, Type.RANDOM, display).sort(new InsertSort());
         // new ArrayIniter(length, Type.RANDOM, display).sort(new QuickSort());
         // new ArrayIniter(length, Type.RANDOM, display).sort(new ShellSort());
         // new ArrayIniter(length, Type.RANDOM, display).sort(new MergeSort());
         // new ArrayIniter(length, Type.RANDOM, display).sort(new CountSort());
         // new ArrayStringIniter(length, strLength, display).sort(new RadixSort());
-        new ArrayStringIniter(length, strLength,  display).sort(new RadixSort2());
-        new ArrayStringIniter(length, strLength, 1, display).sort(new RadixSort3());
+        // new ArrayStringIniter(length, strLength,  display).sort(new RadixSort2());
+        // new ArrayStringIniter(length, strLength, 1, display).sort(new RadixSort3());
 
 
     }
